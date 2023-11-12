5d67cc99bc6d3b0a975d799b4c25249479ccfefa
jaimetorres <jaime.torres@version1.com>
2023-11-12 20:46:49 +0100

    Event view added

--- lib/widgets/events/event_card.dart
+++ lib/widgets/events/event_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:nsqv/models/event.dart';
+import 'package:nsqv/widgets/events/event_view.dart';
 
 class MovieCard extends StatelessWidget {
   const MovieCard({super.key, required this.event});
@@ -9,25 +10,31 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              if (event.imageUrl.isNotEmpty)
+    return InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => EventView(event: event)),
+      ),
+      child: Card(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
                 Stack(
                   alignment: Alignment.bottomLeft,
                   children: [
                     Center(
                       child: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                event.imageUrl,
-                              ),
-                              fit: BoxFit.fill),
-                        ),
+                        decoration: event.imageUrl.isNotEmpty
+                            ? BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                      event.imageUrl,
+                                    ),
+                                    fit: BoxFit.fill),
+                              )
+                            : null,
                         width: 200,
                         height: 280,
                       ),
@@ -64,59 +71,60 @@ class MovieCard extends StatelessWidget {
                     )
                   ],
                 ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.network(
+                        event.channel.logoUrl,
+                        scale: 1.3,
+                      ),
+                      Text(
+                        '${DateFormat('HH:mm').format(event.start)} - ${DateFormat('HH:mm').format(event.end)}',
+                        style: const TextStyle(fontSize: 12),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
                   children: [
-                    Image.network(
-                      event.channel.logoUrl,
-                      scale: 1.3,
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: LinearProgressIndicator(
+                          value: event.progress / 100,
+                          minHeight: 6,
+                        ),
+                      ),
                     ),
-                    Text(
-                      '${DateFormat('HH:mm').format(event.start)} - ${DateFormat('HH:mm').format(event.end)}',
-                      style: const TextStyle(fontSize: 12),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: Text('${event.progress}%'),
                     )
                   ],
                 ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: LinearProgressIndicator(
-                        value: event.progress / 100,
-                        minHeight: 6,
-                      ),
+              ],
+            ),
+            if (event.rate > 0)
+              Container(
+                color: const Color.fromARGB(180, 0, 0, 0),
+                width: 40,
+                height: 40,
+                child: Center(
+                  child: Text(
+                    event.rate.toStringAsFixed(1),
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 15),
-                    child: Text('${event.progress}%'),
-                  )
-                ],
-              ),
-            ],
-          ),
-          if (event.rate > 0)
-            Container(
-              color: const Color.fromARGB(180, 0, 0, 0),
-              width: 40,
-              height: 40,
-              child: Center(
-                child: Text(
-                  event.rate.toStringAsFixed(1),
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
                   ),
                 ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/events/event_view.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:nsqv/models/event.dart';
+import 'package:nsqv/models/event_type.dart';
+
+class EventView extends StatelessWidget {
+  const EventView({super.key, required this.event});
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: ListView(
+        children: [
+          Center(
+            child: event.imageUrl.isNotEmpty
+                ? Stack(
+                    children: [
+                      Image.network(
+                        event.imageUrl,
+                      ),
+                      Container(
+                        color: const Color.fromARGB(180, 0, 0, 0),
+                        height: 50,
+                        width: 50,
+                        child: Center(
+                          child: Text(
+                            event.rate.toStringAsFixed(1),
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).colorScheme.onPrimary),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                : Container(
+                    color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    width: double.infinity,
+                    height: 300,
+                    child: const Center(
+                      child: Text(
+                        "No hay imagen",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+            ),
+            child: Text(
+              event.name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Theme.of(context).colorScheme.onPrimary),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              event.chapterName,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: Theme.of(context).colorScheme.onPrimary),
+            ),
+          ),
+          Text(
+            event.classification,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+                color: Theme.of(context).colorScheme.onPrimary),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
+            width: double.infinity,
+            child: Text(
+              'Canal: ${event.channel.name}',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
+            width: double.infinity,
+            child: Text(
+              'Duración: ${event.duration} minutos',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          if (event.eventType == EventType.movie)
+            ..._movieView(context)
+          else
+            ..._serieView(context)
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.arrow_back,
+        ),
+        onPressed: () => Navigator.pop(context),
+      ),
+    );
+  }
+
+  List<Widget> _movieView(BuildContext context) => [
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          width: double.infinity,
+          child: Text(
+            'Director: ${event.director.substring(0, event.director.length - 2)}',
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          width: double.infinity,
+          child: Text(
+            'Intérpretes: ${event.interpreters}',
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          width: double.infinity,
+          child: Text(
+            'Sinopsis: ${event.synopsis}',
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+      ];
+
+  List<Widget> _serieView(BuildContext context) => [
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          width: double.infinity,
+          child: Text(
+            'Sinopsis: ${event.synopsis}',
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+      ];
+}
